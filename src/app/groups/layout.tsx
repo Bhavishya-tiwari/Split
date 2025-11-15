84a76055dba5563266a537d8da025f41909a5021
cb-bhavishya-tiwari <bhavishya.tiwari@chargebee.com>
2025-11-16 02:48:03 +0530

    me

--- src/app/groups/layout.tsx
+++ src/app/groups/layout.tsx
@@ -7,7 +7,7 @@ import SideMenu from '@/components/SideMenu';
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+// todo 
   return (
     <div className="min-h-screen bg-gradient-to-br from-emerald-50 to-teal-50 flex flex-col pb-20">
       <Navbar onMenuOpen={() => setIsMenuOpen(true)} />
